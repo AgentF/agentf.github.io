6221d304a80db993afe277ddf3d9e99d2585cbef
AgentF <freddyfx231@gmail.com>
2019-08-14 18:18:44 -0400

    changed files structure and reorganized package.json

--- src/components/App/View.jsx
+++ src/components/App/View.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from '../../assets/logo.svg';
 import './App.css';
 
-function App() {
+const View = () => {
   return (
     <div className="App">
       <header className="App-header">
@@ -23,4 +23,4 @@ function App() {
   );
 }
 
-export default App;
+export default View;
