2e359a9757cbf0399caf44d90c514a376e2cd376
AgentF <freddyfx231@gmail.com>
2019-08-15 22:22:48 -0400

    Updates

--- README.md
+++
@@ -1,3 +0,0 @@
-### AgentF
-
-Hello there! I'm Freddy Mota a Web Developer, nice to meet you this will be my portfolio page for my projects in GitHub
---
+++ precache-manifest.45774661708d0f85fa18caea38b7b4f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee20be5343c1cae12fdfd2b490041615",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b9023222e7615b3ed837",
+    "url": "/static/css/main.5054f657.chunk.css"
+  },
+  {
+    "revision": "b15850353a12ff9a963b",
+    "url": "/static/js/2.817214f0.chunk.js"
+  },
+  {
+    "revision": "b9023222e7615b3ed837",
+    "url": "/static/js/main.65d3ca14.chunk.js"
+  },
+  {
+    "revision": "cb405c0181855f2066a0",
+    "url": "/static/js/runtime~main.4215ef20.js"
+  },
+  {
+    "revision": "88cce291498e594d5c87865cfb998ecb",
+    "url": "/static/media/undraw_web_developer_p3e5.88cce291.svg"
+  }
+]);
